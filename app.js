418e1f10aabf0da2aaa7284d378ba130c80bceb2
driesdroesbeke <dries.droesbeke@digipolis.be>
2014-10-03 17:10:03 +0200

    update app

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
+/************************************
+ **** INIT * ************************
+ ************************************/
 var webclient = require('https');
 var fs = require('fs');
-//var tessel = require('tessel');
-// var audio = require('audio-vs1053b').use(tessel.port['A']);
+var tessel = require('tessel');
+var audio = require('audio-vs1053b').use(tessel.port['A']);
 
-
+//disable log when log config is false
 var fileContents = fs.readFileSync("config.json");
 var obj_config = JSON.parse(fileContents);
 
-
-var int_timer = 5000;
-
-
-//overwrite or live environment
-/*if(overwrite == true) {
+if(!obj_config.log ) {
  console.log = function () {};
- }*/
+}
 
-//get data over http
+
+
+/************************************
+ **** Functions *********************
+ ************************************/
 function fnGetData() {
-    console.log("the fuck");
+    console.log("fnGetData");
     try {
         var options = {
             host: 'api.solidshops.com',
             port: 443,
-            path: '/v1/orders.json',
+            path: '/v1/orders.json?count=1&order=id:desc',
             headers: {
                 'Authorization': 'Basic '
                     + new Buffer(obj_config.api.key + ':' + obj_config.api.secret).toString('base64'),
@@ -36,29 +38,27 @@ function fnGetData() {
             var body = "";
             res.on('data', function (data) {
                 body += data;
-                //res.resume();
             });
             res.on('end', function () {
                 // here we have the full response, html or json object
-                // console.log(body);
                 try {
                     var obj_response = JSON.parse(body);
                     fnHandleOrder(obj_response.data[0]);
                 } catch (e) {
-                    console.log('error in fnGetData parse data');
+                    console.log("Error:  res.on('end') " + e.message);
                 }
             })
             res.on('error', function (e) {
-                console.log("Got error: " + e.message);
+                console.log("Error:  res.on('error') " + e.message);
             });
         });
-        request.on("error", function (error) {
-            console.log("an error occured during the request");
+        request.on('error', function (error) {
+            console.log("Error: request.on('error')");
         });
 
         request.end();
     } catch (e) {
-        console.log('error in fnGetData http connection');
+        console.log('Error in fnGetData http connection');
     }
 
 
@@ -68,7 +68,6 @@ function fnGetData() {
 var id_order_latest = 0;
 function fnHandleOrder(obj_order) {
     //check order data
-    console.log(obj_order.id,id_order_latest )
     if (parseInt(obj_order.id) > id_order_latest) {
         id_order_latest = parseInt(obj_order.id);
         console.log("a new order was found: " + id_order_latest);
@@ -79,37 +78,31 @@ function fnHandleOrder(obj_order) {
 //play sound
 function fnPlayNotification(str_filename) {
     var song = fs.readFileSync(str_filename);
-    /*audio.play(song, function(err) {
+    audio.play(song, function(err) {
      if (err) {
-     console.log(err);
+        console.log(err);
      } else {
-     console.log("Start playing: " +str_filename);
+        console.log("Start playing: " +str_filename);
      }
-     });*/
+     });
 }
 
 
-/*
+/************************************
+ **** Start *************************
+ ************************************/
+
  audio.on('ready', function() {
- audio.setVolume(10, function(err) {
- console.log("Volumse set");
- //just play something so you know the app has started
- fnPlayNotification("sound/neworder.mp3");
+    audio.setVolume(10, function(err) {
+        console.log("Volumse set");
+        //just play something so you know the app has started
+        fnPlayNotification("sound/neworder.mp3");
+        //run after 5 seconds after app is completely booted
+        setTimeout(function () {
+            setInterval(fnGetData, obj_config.interval);
+        }, 5);
+    });
  });
- });
- */
-
-
-
-
-
-//run after 10 seconds so audio is set
-setTimeout(function () {
-    setInterval(fnGetData, int_timer);
-}, 10);
-
-
-
 
 
 
